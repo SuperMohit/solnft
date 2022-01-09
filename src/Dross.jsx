e7daa14da65a30700c6741b60f170f091a6b87e2
Mohit Talniya <mohittalniya@gmail.com>
2022-01-08 20:57:23 -0500

    Add files via upload

---
+++ src/Dross.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+function Dross(){
+    return <h1 class="op9">All Collections</h1>;}
+export default Dross;
---
+++ src/Pross.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+function Pross(){
+    return <h1 class="op">Top Collection In 7 Days</h1>;}
+export default Pross;
